5e1550a07ebd446de90e70768e02084428431ce6
Luckoo <beobsung1@gmail.com>
2022-03-27 16:44:52 +0900

    auto py 삭제

--- auto_py/youtube_download_list.py
+++
@@ -1,7 +0,0 @@
-import pytube
-
-link = "https://www.youtube.com/playlist?list=PLZOm4uzWk9WP"
-p = pytube.Playlist(link)
-for video in p.videos:
-    video = video.streams.get_highest_resolution()
-    video.download()
