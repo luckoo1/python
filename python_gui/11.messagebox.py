6b8548676cebd8b4bc8285b17d1914a159f7856c
Luckoo <beobsung1@gmail.com>
2022-04-04 20:27:54 +0900

    12장

--- python_gui/11.messagebox.py
+++ python_gui/11.messagebox.py
@@ -1,35 +1,27 @@
 import tkinter.messagebox as msgbox
 from tkinter import *
-from urllib import response
 
 root = Tk()
 root.title("Beobsung GUI")
 root.geometry("640x480")
 
-
 def info():
     msgbox.showinfo("알림", "정상적으로 예매 완료되었습니다.")
-
 
 def warn():
     msgbox.showwarning("경고", "해당좌석은 매진되었습니다.")
 
-
 def error():
     msgbox.showerror("에러", "결제 오류가 발생했습니다.")
-
 
 def okcancel():
     msgbox.askokcancel("확인/취소", "해당 좌석은 유아동반석입니다. 예매하시겠습니까?")
 
-
 def retrycancel():
     msgbox.askretrycancel("재시도/취소", "일시적인 오류입니다. 다시 시도하시겠습니까?")
 
-
 def yesno():
     msgbox.askyesno("예/아니요", "예당 좌석은 역방향입니다. 예매하시겠습니까?")
-
 
 def yesnocancel():
     response = msgbox.askyesnocancel(title=None, message="예매 내역이 저장되지 안았습니다.\n저장후 프로그램을 종료하시겠습니까?")
